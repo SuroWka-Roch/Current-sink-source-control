fc7c60df78b240822fa8aafa53a0dae49db2ad55
piernik <wojtek@surowka.com>
2018-11-02 18:03:05 +0100

    programowanie u rodziny

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.5
+
+#Program Inżynierki Wojciecha Surówki.
+
+from mine import createGuiWindow
+try:
+  with open('defValues','r') as file:
+    defaultTable = [val.strip('\n') for val in file]
+    if len(defaultTable)>8:
+      defaultTable = defaultTable[0:8]
+except Exception:
+  defaultTable = [int(4000/7* i) for i in range(8)]
+createGuiWindow(defaultTable)
--- mine.py
+++ mine.py
@@ -4,92 +4,94 @@ from tkinter import *
 from tkinter import ttk
 from serialInfo import serial_ports
 import gui_function
+def createGuiWindow(defaultTable):
+  """make the gui"""
 
-# Base window creation.
+  # Base window creation.
 
-root = Tk()
-root.title("Inżynierka")
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+  root = Tk()
+  root.title("Inżynierka")
+  root.columnconfigure(0, weight=1)
+  root.rowconfigure(0, weight=1)
 
-# Set frame to add widgets to.
+  # Set frame to add widgets to.
 
-mainframe = ttk.Frame(root, padding="6 6 6 6")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+  mainframe = ttk.Frame(root, padding="6 6 6 6")
+  mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-# Handle streathing
-for i in range(8):
-  mainframe.rowconfigure(i, weight=1)
-for i in range(6):
-  mainframe.columnconfigure(i,weight=1)
+  # Handle streathing
+  for i in range(8):
+    mainframe.rowconfigure(i, weight=1)
+  for i in range(6):
+    mainframe.columnconfigure(i,weight=1)
 
-# Window valuables.
-state = StringVar()
-state.set("Miejsce informacji")
-portValues = [StringVar() for _ in range(8)]
+  # Window valuables.
+  state = StringVar()
+  state.set("Miejsce informacji")
+  portValues = [StringVar() for _ in range(8)]
 
-# Constants
-padingx = 5
+  # Constants
+  padingx = 5
 
-# Legend line.
-explanationString = '''Program ten jest częścią pracy inżynierskiej Wojciecha Surówki. 
-Użycie:
-Na rozsuwanym menu wybierz port do którego jest połączony moduł Arduino.
-Następnie wciśnij "conect" w celu sprawdzenia poprawności załadowanego kodu. 
-Po wypełnieniu okienek wartości napięć użyj klawisza "send" żeby załadować napięcia do układu\n'''
+  # Legend line.
+  explanationString = '''Program ten jest częścią pracy inżynierskiej Wojciecha Surówki. 
+  Użycie:
+  Na rozsuwanym menu wybierz port do którego jest połączony moduł Arduino.
+  Następnie wciśnij "conect" w celu sprawdzenia poprawności załadowanego kodu. 
+  Po wypełnieniu okienek wartości napięć użyj klawisza "send" żeby załadować napięcia do układu\n'''
 
-ttk.Label(mainframe, text=explanationString).grid(column=0, row=0, columnspan=6, sticky=(N, W, E, S), pady=5, padx=3)
-ttk.Label(mainframe, text="Port name").grid(column=0, row=1, sticky=W, padx=padingx)
-ttk.Label(mainframe, text="Value").grid(column=1, row=1, sticky=( W, E), padx=padingx)
-ttk.Label(mainframe, text="Unit").grid(column=2, row=1, sticky=E, padx=padingx)
-ttk.Label(mainframe, text="Port name").grid(column=3, row=1, sticky=W, padx=padingx)
-ttk.Label(mainframe, text="Value").grid(column=4, row=1, sticky=( W, E), padx=padingx)
-ttk.Label(mainframe, text="Unit").grid(column=5, row=1, sticky=E, padx=padingx)
+  ttk.Label(mainframe, text=explanationString).grid(column=0, row=0, columnspan=6, sticky=(N, W, E, S), pady=5, padx=3)
+  ttk.Label(mainframe, text="Port name").grid(column=0, row=1, sticky=W, padx=padingx)
+  ttk.Label(mainframe, text="Value").grid(column=1, row=1, sticky=( W, E), padx=padingx)
+  ttk.Label(mainframe, text="Unit").grid(column=2, row=1, sticky=E, padx=padingx)
+  ttk.Label(mainframe, text="Port name").grid(column=3, row=1, sticky=W, padx=padingx)
+  ttk.Label(mainframe, text="Value").grid(column=4, row=1, sticky=( W, E), padx=padingx)
+  ttk.Label(mainframe, text="Unit").grid(column=5, row=1, sticky=E, padx=padingx)
 
-# Port name line
+  # Port name line
 
-ttk.Label(mainframe, text="1").grid(column=0, row=2, sticky=W, padx=padingx)
-ttk.Label(mainframe, text="2").grid(column=0, row=3, sticky=W, padx=padingx)
-ttk.Label(mainframe, text="3").grid(column=0, row=4, sticky=W, padx=padingx)
-ttk.Label(mainframe, text="4").grid(column=3, row=2, sticky=W, padx=padingx)
-ttk.Label(mainframe, text="5").grid(column=3, row=3, sticky=W, padx=padingx)
-ttk.Label(mainframe, text="6").grid(column=3, row=4, sticky=W, padx=padingx)
-ttk.Label(mainframe, text="7").grid(column=0, row=5, sticky=W, padx=padingx)
-defaultTable = [int(4000/7* i) for i in range(8)]
-entryList = []
-for i in range(8):
-  entryList.append(Entry(mainframe, width=10, textvariable=portValues[i]))
-  entryList[i].grid(column=1 if i < 4 else 4, row=i+2 if i < 4 else i-2)
-  portValues[i].set(defaultTable[i])
+  ttk.Label(mainframe, text="1").grid(column=0, row=2, sticky=W, padx=padingx)
+  ttk.Label(mainframe, text="2").grid(column=0, row=3, sticky=W, padx=padingx)
+  ttk.Label(mainframe, text="3").grid(column=0, row=4, sticky=W, padx=padingx)
+  ttk.Label(mainframe, text="4").grid(column=3, row=2, sticky=W, padx=padingx)
+  ttk.Label(mainframe, text="5").grid(column=3, row=3, sticky=W, padx=padingx)
+  ttk.Label(mainframe, text="6").grid(column=3, row=4, sticky=W, padx=padingx)
+  ttk.Label(mainframe, text="7").grid(column=0, row=5, sticky=W, padx=padingx)
 
-# Unit line.
-ttk.Label(mainframe, text="µA").grid(column=2, row=2, sticky=E, padx=padingx)
-ttk.Label(mainframe, text="µA").grid(column=2, row=3, sticky=E, padx=padingx)
-ttk.Label(mainframe, text="µA").grid(column=2, row=4, sticky=E, padx=padingx)
-ttk.Label(mainframe, text="µA").grid(column=2, row=5, sticky=E, padx=padingx)
+  entryList = []
+  for i in range(8):
+    entryList.append(Entry(mainframe, width=10, textvariable=portValues[i]))
+    entryList[i].grid(column=1 if i < 4 else 4, row=i+2 if i < 4 else i-2)
+    portValues[i].set(defaultTable[i])
 
-ttk.Label(mainframe, text="µA").grid(column=5, row=2, sticky=E, padx=padingx)
-ttk.Label(mainframe, text="µA").grid(column=5, row=3, sticky=E, padx=padingx)
-ttk.Label(mainframe, text="µA").grid(column=5, row=4, sticky=E, padx=padingx)
-ttk.Label(mainframe, text="µA").grid(column=5, row=5, sticky=E, padx=padingx)
+  # Unit line.
+  ttk.Label(mainframe, text="µA").grid(column=2, row=2, sticky=E, padx=padingx)
+  ttk.Label(mainframe, text="µA").grid(column=2, row=3, sticky=E, padx=padingx)
+  ttk.Label(mainframe, text="µA").grid(column=2, row=4, sticky=E, padx=padingx)
+  ttk.Label(mainframe, text="µA").grid(column=2, row=5, sticky=E, padx=padingx)
 
-# status and conection
-usbList = serial_ports()
-if not usbList:
-  usbList=['none found']
-  state.set("check if arduino is conected")
-usbList.append("reset")
+  ttk.Label(mainframe, text="µA").grid(column=5, row=2, sticky=E, padx=padingx)
+  ttk.Label(mainframe, text="µA").grid(column=5, row=3, sticky=E, padx=padingx)
+  ttk.Label(mainframe, text="µA").grid(column=5, row=4, sticky=E, padx=padingx)
+  ttk.Label(mainframe, text="µA").grid(column=5, row=5, sticky=E, padx=padingx)
 
-ttk.Label(mainframe, textvariable=state).grid(column=0, row=6, columnspan=2)
-ttk.Label(mainframe, text='port').grid(column=2, row=6,sticky=E)
-usbPortUsersChose = StringVar()
-comboBox = ttk.Combobox(mainframe, textvariable=usbPortUsersChose,values=usbList)
-comboBox.grid(column=3, row=6, columnspan=2)
-comboBox.set(usbList[0])
-ttk.Button(mainframe, text='connect', command=lambda: gui_function.ConnectButtonFunction(usbPortUsersChose.get(),state,comboBox)).grid(column=5, row=6)
+  # status and conection
+  usbList = serial_ports()
+  if not usbList:
+    usbList=['none found']
+    state.set("check if arduino is conected")
+  usbList.append("reset")
 
-#send buttom
+  ttk.Label(mainframe, textvariable=state).grid(column=0, row=6, columnspan=2)
+  ttk.Label(mainframe, text='port').grid(column=2, row=6,sticky=E)
+  usbPortUsersChose = StringVar()
+  comboBox = ttk.Combobox(mainframe, textvariable=usbPortUsersChose,values=usbList)
+  comboBox.grid(column=3, row=6, columnspan=2)
+  comboBox.set(usbList[0])
+  ttk.Button(mainframe, text='connect', command=lambda: gui_function.ConnectButtonFunction(usbPortUsersChose.get(),state,comboBox)).grid(column=5, row=6)
 
-ttk.Button(mainframe, text='send', command=lambda: gui_function.SendButtonFunction(portValues,usbPortUsersChose.get())).grid(column=4, row=7, columnspan=2, sticky= (W,E))
+  #send buttom
 
-root.mainloop()
+  ttk.Button(mainframe, text='send', command=lambda: gui_function.SendButtonFunction(portValues,usbPortUsersChose.get())).grid(column=4, row=7, columnspan=2, sticky= (W,E))
+
+  root.mainloop()
